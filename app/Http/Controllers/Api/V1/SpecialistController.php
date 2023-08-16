819a0bb971a73b6ce652c66e1549cb72ee6dd242
Andrey Bulavskiy <10vvvvvv31@gmail.com>
2023-08-16 16:45:25 +0300

    generate specialist files

---
+++ app/Http/Controllers/Api/V1/SpecialistController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSpecialistRequest;
+use App\Http\Requests\UpdateSpecialistRequest;
+use App\Models\Specialist;
+
+class SpecialistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSpecialistRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Specialist $specialist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSpecialistRequest $request, Specialist $specialist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Specialist $specialist)
+    {
+        //
+    }
+}
