ac0544dd0d8717e58edd53bafafab58b9b5523df
Andrey Bulavskiy <10vvvvvv31@gmail.com>
2023-08-16 16:12:43 +0300

    move client controller into Api/V1

--- app/Http/Controllers/Api/V1/ClientController.php
+++ app/Http/Controllers/Api/V1/ClientController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreClientRequest;
 use App\Http\Requests\UpdateClientRequest;
 use App\Models\Client;
--- routes/version/v1.php
+++ routes/version/v1.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ClientController;
+use App\Http\Controllers\Api\V1\ClientController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function() {
